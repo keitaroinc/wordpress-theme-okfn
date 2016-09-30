b48ce07bdd25d4340217618053123095d1d79e19
gmitevski <goce.mitevski@gmail.com>
2016-09-30 18:16:23 +0200

    Create [twitter_icon] shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -11,14 +11,12 @@
 function pseudocontent_shortcode($atts, $content = null) {
   return '<style type="text/css">#content {width: 100%;} #content #sidebar h5 {margin-top:0px;}</style>
 <article class="span8" style="margin-left:0px;">' . do_shortcode($content) . '</article>';
-
 }
 
 add_shortcode('pseudocontent', 'pseudocontent_shortcode');
 
 function pseudosidebar_shortcode($atts, $content = null) {
   return '<div id="sidebar" role="complementary" class="span4 pseudo-sidebar">' . $content . '</div>';
-
 }
 
 add_shortcode('pseudosidebar', 'pseudosidebar_shortcode');
@@ -38,23 +36,22 @@ add_shortcode('pseudosidebar', 'pseudosidebar_shortcode');
 
 function carousel_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'class' => '',
-          ), $atts));
+      'class' => '',
+                  ), $atts));
   return '<div id="myCarousel" class="carousel slide ' . $class . '"><div class="carousel-inner">' . do_shortcode($content) . '</div><a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a><a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a></div><script>$("div.text-slide, div.calendar-slide").parent().addClass("not-photo");</script>';
-
 }
 
 add_shortcode('carousel', 'carousel_shortcode');
 
 function carousel_slide_shortcode($atts) {
   extract(shortcode_atts(array(
-    'img' => '',
-    'text' => '',
-    'caption' => '',
-    'class' => '',
-    'heading' => '',
-    'calendar' => '',
-          ), $atts));
+      'img' => '',
+      'text' => '',
+      'caption' => '',
+      'class' => '',
+      'heading' => '',
+      'calendar' => '',
+                  ), $atts));
 
   if (!empty($calendar)) {
     $googleCalendar = '[google-calendar-events id="' . $calendar . '" type="ajax"]';
@@ -84,7 +81,6 @@ function carousel_slide_shortcode($atts) {
   $slide = $slideOpen . $slideContent . $slideCaptionOpen . $slideHeading . $slideCaption . $slideClose;
 
   return $slide;
-
 }
 
 add_shortcode('slide', 'carousel_slide_shortcode');
@@ -103,17 +99,15 @@ add_shortcode('slide', 'carousel_slide_shortcode');
 
 function zcarousel_shortcode($atts, $content = null) {
   return '<div id="zcarousel" style="width: 940px; height: 250px; "></div><script>var data=[];' . do_shortcode($content) . 'jQuery("#zcarousel").zcarousel(data);</script>';
-
 }
 
 add_shortcode('zcarousel', 'zcarousel_shortcode');
 
 function zcarousel_slide_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'img' => 'http://farm8.staticflickr.com/7174/6554801385_83acdc501d_o_d.png',
-          ), $atts));
+      'img' => 'http://farm8.staticflickr.com/7174/6554801385_83acdc501d_o_d.png',
+                  ), $atts));
   return 'data.push({"url":"' . $img . '","caption":"' . $content . '"});';
-
 }
 
 add_shortcode('zslide', 'zcarousel_slide_shortcode');
@@ -132,14 +126,13 @@ add_shortcode('banner', 'banner_shortcode');
 
 function banner_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'bg' => 'http://assets.okfn.org/web/images/banner.png',
-    'height' => '320',
-    'bgcolour' => 'd4d4d4',
-    'class' => ''
-          ), $atts));
+      'bg' => 'http://assets.okfn.org/web/images/banner.png',
+      'height' => '320',
+      'bgcolour' => 'd4d4d4',
+      'class' => ''
+                  ), $atts));
   $padheight = $height - 40;
   return '<div class="static-banner ' . $class . '" style="background-image:url(' . $bg . '); height:' . $padheight . 'px; background-color:#' . $bgcolour . ';"><div class="inner">' . do_shortcode($content) . '</div></div>';
-
 }
 
 add_shortcode('banner', 'banner_shortcode');
@@ -153,7 +146,6 @@ add_shortcode('banner', 'banner_shortcode');
 
 function notitle_shortcode($atts) {
   return '<style type="text/css"> .pagetitle { display: none; } </style>';
-
 }
 
 add_shortcode('notitle', 'notitle_shortcode');
@@ -167,7 +159,6 @@ add_shortcode('notitle', 'notitle_shortcode');
 
 function fullwidth_shortcode($atts) {
   return '<script>$("#content").addClass("fullwidth");</script>';
-
 }
 
 add_shortcode('fullwidth', 'fullwidth_shortcode');
@@ -189,34 +180,32 @@ add_shortcode('fullwidth', 'fullwidth_shortcode');
 
 function row_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'class' => '',
-    'style' => '',
-          ), $atts));
+      'class' => '',
+      'style' => '',
+                  ), $atts));
   $styles = '';
   if (!empty($style)) {
     $styles = ' style="' . $style . '"';
   }
 
   return '<div class="row ' . $class . '"' . $styles . '>' . do_shortcode($content) . '</div>';
-
 }
 
 add_shortcode('row', 'row_shortcode');
 
 function column_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'span' => '12',
-    'offset' => '0',
-    'class' => '',
-    'style' => '',
-          ), $atts));
+      'span' => '12',
+      'offset' => '0',
+      'class' => '',
+      'style' => '',
+                  ), $atts));
   $styles = '';
   if (!empty($style)) {
     $styles = ' style="' . $style . '"';
   }
 
   return '<div class="span' . $span . ' offset' . $offset . ' ' . $class . '"' . $styles . '>' . do_shortcode($content) . '</div>';
-
 }
 
 add_shortcode('column', 'column_shortcode');
@@ -231,7 +220,6 @@ add_shortcode('column', 'column_shortcode');
 
 function clear_shortcode($atts) {
   return '<br style="clear:both;" />';
-
 }
 
 add_shortcode('clear', 'clear_shortcode');
@@ -250,22 +238,21 @@ add_shortcode('clear', 'clear_shortcode');
 
 function gridlist_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'columns' => '3',
-          ), $atts));
+      'columns' => '3',
+                  ), $atts));
   return '<dl class="grid-list columns' . $columns . '">' . do_shortcode($content) . '<br style="clear:both;" /></dl>';
-
 }
 
 add_shortcode('gl', 'gridlist_shortcode');
 
 function gridlist_item_shortcode($atts) {
   extract(shortcode_atts(array(
-    'title' => '',
-    'description' => '',
-    'link' => '',
-    'icon' => '',
-    'image' => '',
-          ), $atts));
+      'title' => '',
+      'description' => '',
+      'link' => '',
+      'icon' => '',
+      'image' => '',
+                  ), $atts));
 
   if (!empty($icon) && !empty($link)) {
     return '<a href="' . $link . '" class="well"><dt><img src="' . $icon . '" alt="' . $title . '" class="icon"><div class="title">' . $title . '</div></dt>
@@ -286,7 +273,6 @@ function gridlist_item_shortcode($atts) {
     return '<div class="well"><dt>' . $title . '</dt>
 							<dd>' . $description . '</dd></div>';
   }
-
 }
 
 add_shortcode('gli', 'gridlist_item_shortcode');
@@ -303,13 +289,13 @@ if (function_exists('base_rss_feed') && !function_exists('base_rss_shortcode')) 
 
   function base_rss_shortcode($atts) {
     extract(shortcode_atts(array(
-      'size' => '10',
-      'feed' => 'http://wordpress.org/news/feed/',
-      'date' => false,
-      'class' => '',
-      'id' => '1',
-      'type' => '',
-            ), $atts));
+        'size' => '10',
+        'feed' => 'http://wordpress.org/news/feed/',
+        'date' => false,
+        'class' => '',
+        'id' => '1',
+        'type' => '',
+                    ), $atts));
 
     $content = base_rss_feed($size, $feed, $date);
     if ($type == 'ticker') {
@@ -323,7 +309,6 @@ if (function_exists('base_rss_feed') && !function_exists('base_rss_shortcode')) 
     } else {
       return '<div class="rss size' . $size . ' ' . $class . '">' . $content . '</div>';
     }
-
   }
 
   add_shortcode("rss", "base_rss_shortcode");
@@ -342,21 +327,20 @@ if (function_exists('base_rss_feed') && !function_exists('base_rss_shortcode')) 
 
 function accordions_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'class' => 'accordion',
-          ), $atts));
+      'class' => 'accordion',
+                  ), $atts));
   STATIC $id = 0;
   $id++;
   return '<div id="accordion' . $id . '" class="' . $class . '">' . do_shortcode($content) . '</div>';
-
 }
 
 add_shortcode('accordions', 'accordions_shortcode');
 
 function accordion_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'heading' => 'Please enter a heading attribute like [accordion heading="My Heading"]',
-    'class' => '',
-          ), $atts));
+      'heading' => 'Please enter a heading attribute like [accordion heading="My Heading"]',
+      'class' => '',
+                  ), $atts));
   STATIC $collapse = 0;
   $collapse++;
   return '<div class="accordion-group">
@@ -371,7 +355,6 @@ function accordion_shortcode($atts, $content = null) {
 							</div>
 					  </div>
 					</div>';
-
 }
 
 add_shortcode('accordion', 'accordion_shortcode');
@@ -385,10 +368,10 @@ add_shortcode('accordion', 'accordion_shortcode');
 
 function sticky_shortcode($atts) {
   extract(shortcode_atts(array(
-    'scroll' => '65',
-    'class' => 'subnav',
-    'id' => '$el'
-          ), $atts));
+      'scroll' => '65',
+      'class' => 'subnav',
+      'id' => '$el'
+                  ), $atts));
   return "<script>$(window).scroll(function(e){
   " . $id . " = $('." . $class . "');
   if ($(this).scrollTop() > " . $scroll . " && " . $id . ".css('position') != 'fixed'){
@@ -402,7 +385,6 @@ function sticky_shortcode($atts) {
 });
 $('body').addClass('sticky-" . $class . " top');
 </script>";
-
 }
 
 add_shortcode('sticky', 'sticky_shortcode');
@@ -417,8 +399,8 @@ add_shortcode('sticky', 'sticky_shortcode');
 function pippin_login_form_shortcode($atts, $content = null) {
 
   extract(shortcode_atts(array(
-    'redirect' => ''
-          ), $atts));
+      'redirect' => ''
+                  ), $atts));
 
   if (!is_user_logged_in()) {
     if ($redirect) {
@@ -429,7 +411,6 @@ function pippin_login_form_shortcode($atts, $content = null) {
     $form = wp_login_form(array('echo' => false, 'redirect' => $redirect_url));
   }
   return $form;
-
 }
 
 add_shortcode('loginform', 'pippin_login_form_shortcode');
@@ -446,33 +427,31 @@ add_shortcode('loginform', 'pippin_login_form_shortcode');
 
 function menupod_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'icon' => 'cog',
-    'link' => '#',
-    'heading' => 'Heading Here',
-    'subheading' => '',
-          ), $atts));
+      'icon' => 'cog',
+      'link' => '#',
+      'heading' => 'Heading Here',
+      'subheading' => '',
+                  ), $atts));
   return '<div class="okfn-dropdown">
 		 <a href="' . $link . '" class="background-' . $icon . '">
 			<h5>' . $heading . '</h5>
 			<p>' . $subheading . '</p>
 		</a>
 		<div class="okfn-dropdown-items">'
-      . do_shortcode($content) .
-      '</div>
+          . do_shortcode($content) .
+          '</div>
 	</div>';
-
 }
 
 add_shortcode('menupod', 'menupod_shortcode');
 
 function menupoditem_shortcode($atts) {
   extract(shortcode_atts(array(
-    'text' => 'Text here',
-    'link' => '#',
-          ), $atts));
+      'text' => 'Text here',
+      'link' => '#',
+                  ), $atts));
 
   return '<a href="' . $link . '">' . $text . '</a>';
-
 }
 
 add_shortcode('menupoditem', 'menupoditem_shortcode');
@@ -486,13 +465,13 @@ add_shortcode('menupoditem', 'menupoditem_shortcode');
 
 function latest_posts_shortcode($atts) {
   extract(shortcode_atts(array(
-    'postnumber' => '3',
-    'category' => '',
-    'class' => '',
-          ), $atts));
+      'postnumber' => '3',
+      'category' => '',
+      'class' => '',
+                  ), $atts));
 
   $q = new WP_Query(
-      array('orderby' => 'date', 'posts_per_page' => '' . $postnumber . '', 'category_name' => '' . $category . '')
+          array('orderby' => 'date', 'posts_per_page' => '' . $postnumber . '', 'category_name' => '' . $category . '')
   );
 
   $list = '<ul class="latest-posts posts' . $postnumber . ' ' . $class . '">';
@@ -519,7 +498,6 @@ function latest_posts_shortcode($atts) {
 				});
 		});
 </script>';
-
 }
 
 add_shortcode('latest_posts', 'latest_posts_shortcode');
@@ -531,15 +509,15 @@ add_shortcode('latest_posts', 'latest_posts_shortcode');
 function latest_post_shortcode($atts) {
 
   extract(shortcode_atts(array(
-    'id' => '',
-    'category' => '',
-    'class' => '',
-          ), $atts));
+      'id' => '',
+      'category' => '',
+      'class' => '',
+                  ), $atts));
 
   $latest_post = '';
 
   $q = new WP_Query(
-      array('posts_per_page' => 1, 'category_name' => $category, 'p' => $id)
+          array('posts_per_page' => 1, 'category_name' => $category, 'p' => $id)
   );
 
   while ($q->have_posts()) : $q->the_post();
@@ -570,7 +548,6 @@ function latest_post_shortcode($atts) {
   wp_reset_query();
 
   return $latest_post;
-
 }
 
 add_shortcode('latest_post', 'latest_post_shortcode');
@@ -584,10 +561,10 @@ add_shortcode('latest_post', 'latest_post_shortcode');
 
 function fbanner_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-    'id' => '50136062@N03',
-    'set' => '72157631690090162',
-    'link' => ''
-          ), $atts));
+      'id' => '50136062@N03',
+      'set' => '72157631690090162',
+      'link' => ''
+                  ), $atts));
 
   if ($link == y) {
     $open = '<a href="http://www.flickr.com/photos/' . $id . '/sets/' . $set . '/show/" class="flickr banner"><span class="inner">';
@@ -596,12 +573,12 @@ function fbanner_shortcode($atts, $content = null) {
   }
   ob_start();
   get_flickrRSS(
-      array(
-        'set' => $set,
-        'num_items' => 18,
-        'type' => 'set',
-        'id' => $id,
-      )
+          array(
+              'set' => $set,
+              'num_items' => 18,
+              'type' => 'set',
+              'id' => $id,
+          )
   );
   $images = ob_get_clean();
   if ($link == y) {
@@ -611,7 +588,6 @@ function fbanner_shortcode($atts, $content = null) {
   }
   $banner = $open . $images . $close;
   return $banner;
-
 }
 
 add_shortcode('fbanner', 'fbanner_shortcode');
@@ -625,11 +601,11 @@ add_shortcode('fbanner', 'fbanner_shortcode');
 
 function himg_shortcode($atts) {
   extract(shortcode_atts(array(
-    'image' => '',
-    'break' => '',
-    'width' => '340',
-    'offset' => '0'
-          ), $atts));
+      'image' => '',
+      'break' => '',
+      'width' => '340',
+      'offset' => '0'
+                  ), $atts));
   if (!empty($image)) {
     $bgimg = 'style="background-image:url(' . $image . '); background-position: ' . $offset . 'px bottom;"';
   } else {
@@ -642,7 +618,6 @@ $(".pagetitle").html(html);</script>';
   } else {
     return '<div class="himg" ' . $bgimg . '></div><style>#content h1.pagetitle {position:absolute;right:30px;top:10px;width:' . $width . 'px;height:96px;overflow:hidden;text-align:right;text-transform:uppercase;font-size:36px;line-height:31px;}#content h1.pagetitle:first-line {color:#6a6a6a;}</style>';
   }
-
 }
 
 add_shortcode('himg', 'himg_shortcode');
@@ -656,11 +631,11 @@ add_shortcode('himg', 'himg_shortcode');
 
 function mailman_shortcode($atts) {
   extract(shortcode_atts(array(
-    'domain' => 'http://lists.okfn.org',
-    'list' => '',
-    'button' => 'Subscribe',
-    'popup' => ''
-          ), $atts));
+      'domain' => 'http://lists.okfn.org',
+      'list' => '',
+      'button' => 'Subscribe',
+      'popup' => ''
+                  ), $atts));
 
   if (!empty($popup)) {
     $mailman = '<div class="' . $popup . ' mailman-popup"><div class="icon"></div><p>' . __('Get updates from') . ' ' . get_bloginfo('sitename') . ' ' . __('in your inbox') . '</p><form method="post" action="' . $domain . '/mailman/subscribe/' . $list . '">
@@ -677,7 +652,6 @@ function mailman_shortcode($atts) {
 </form>';
 
   return $mailman;
-
 }
 
 add_shortcode('mailman', 'mailman_shortcode');
@@ -696,20 +670,19 @@ add_shortcode('mailman', 'mailman_shortcode');
 
 function imagelist_shortcode($atts, $content = null) {
   extract(shortcode_atts(array(
-          ), $atts));
+                  ), $atts));
   return '<ul class="image-list">' . do_shortcode($content) . '</ul>';
-
 }
 
 add_shortcode('il', 'imagelist_shortcode');
 
 function imagelist_item_shortcode($atts) {
   extract(shortcode_atts(array(
-    'title' => '',
-    'description' => '',
-    'link' => '',
-    'image' => 'http://assets.okfn.org/web/images/blog-placeholder.png',
-          ), $atts));
+      'title' => '',
+      'description' => '',
+      'link' => '',
+      'image' => 'http://assets.okfn.org/web/images/blog-placeholder.png',
+                  ), $atts));
 
   if (!empty($link)) {
     return '<a href="' . $link . '">
@@ -730,7 +703,6 @@ function imagelist_item_shortcode($atts) {
 							</span>
 							</li>';
   }
-
 }
 
 add_shortcode('ili', 'imagelist_item_shortcode');
@@ -744,8 +716,8 @@ add_shortcode('ili', 'imagelist_item_shortcode');
 
 function scrollme_shortcode($atts) {
   extract(shortcode_atts(array(
-    'duration' => '900',
-          ), $atts));
+      'duration' => '900',
+                  ), $atts));
   return "<script>
    $('a[href^=\"#\"]').on('click',function (e) {
 	    e.preventDefault();
@@ -758,7 +730,6 @@ function scrollme_shortcode($atts) {
 	    });
 	});
 </script>";
-
 }
 
 add_shortcode('scrollme', 'scrollme_shortcode');
@@ -772,10 +743,10 @@ add_shortcode('scrollme', 'scrollme_shortcode');
 
 function tweeter_shortcode($atts) {
   extract(shortcode_atts(array(
-    'total' => '5',
-    'user' => '',
-    'id' => '1',
-          ), $atts));
+      'total' => '5',
+      'user' => '',
+      'id' => '1',
+                  ), $atts));
   $open = '<div id="tweeter' . $id . '" class="tweeter carousel slide ticker">';
   if (!empty($user)) {
     $jmlt = do_shortcode('[jmlt count="' . $total . '" username="' . $user . '"]');
@@ -784,7 +755,13 @@ function tweeter_shortcode($atts) {
   $close = '</div>';
 
   return $open . $jmlt . $close;
-
 }
 
+// Print an SVG Twitter icon where needed
+
 add_shortcode('tweeter', 'tweeter_shortcode');
+add_shortcode('twitter_icon', 'okfn_load_twitter_icon');
+
+function okfn_load_twitter_icon() {
+  return sprintf('<span class="twitter-icon"></span>');
+}
